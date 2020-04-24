83cc733a8480fd40a7ccb68e72d63a6ab18ac566
peofessordeng <2248262060@qq.com>
2020-04-24 11:26:51 +0800

    iframe 宽高比为 16:9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,13 @@
 var iframes = document.getElementsByTagName("iframe");  // 获取 iframe 标签的所有元素
 var Width = iframes[0].offsetWidth;                     // 获取宽度
 for(i = 0; i < iframes.length; i++) {                   // 设置 iframe 元素的高度
-  if (Width < 600) {
-    iframes[i].setAttribute('height', Width / 16 * 9);
-  } else {
-    iframes[i].setAttribute('height', Width / 4 * 3);
-  }
+  iframes[i].setAttribute('height', Width / 16 * 9);
 }
 
 // 窗口监听
 window.onresize = function(){ 
-  var Width = iframes[0].offsetWidth;
-  for(i = 0; i < iframes.length; i++) {
-    if (Width < 600) {
-      iframes[i].setAttribute('height', Width / 16 * 9);
-    } else {
-      iframes[i].setAttribute('height', Width / 4 * 3);
-    }
+  var Width = iframes[0].offsetWidth;                     // 获取宽度
+  for(i = 0; i < iframes.length; i++) {                   // 设置 iframe 元素的高度
+    iframes[i].setAttribute('height', Width / 16 * 9);
   }
 };
