9e8b95a8259caf295d5c64b16af51fc63949538d
peofessordeng <2248262060@qq.com>
2020-05-01 16:33:22 +0800

    滚动时显示回到顶部的按钮

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// 检测滚动栏，显示返回顶部的按钮
+window.onscroll = function(){
+  var t = document.documentElement.scrollTop || document.body.scrollTop; 
+  var top = document.getElementsByClassName("top");
+  if( t >= 300 ) { 
+    top[0].style.display = "block";
+  } else {
+    top[0].style.display = "none";
+  }
+}
