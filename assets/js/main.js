8b3f13bcd1c875b6174d5ae2d0b4506e4a7385f7
peofessordeng <2248262060@qq.com>
2020-05-02 09:35:06 +0800

    透明度调试

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // 检测滚动栏，显示返回顶部的按钮
-window.onscroll = function(){
-  var t = document.documentElement.scrollTop || document.body.scrollTop; 
+window.onscroll = function() {
+  var t = document.documentElement.scrollTop || document.body.scrollTop;
   var top = document.getElementsByClassName("top");
-  if( t >= 300 ) { 
+  if( t > 300 ) { 
     top[0].style.display = "block";
-  } 
+  } else {
+    top[0].style.display = "none";
+  }
 }
