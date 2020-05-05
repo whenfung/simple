9626fb4f59ef45f29a0eba842ff77d7d77a0a65e
peofessordeng <2248262060@qq.com>
2020-05-05 10:20:07 +0800

    边框优化、缓慢滚动、代码优化

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,22 @@ window.onscroll = function() {
     top[0].style.display = "none";
   }
 }
+
+// 检测回到顶部按钮
+window.onload = function(){
+  var top = document.getElementsByClassName("top");
+  top[0].onclick = function() {
+      toScroll();
+  }
+}
+
+function toScroll() {
+  clearInterval(this.timer);
+  this.timer = setInterval(function() {
+    var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+    window.scrollBy(0, -100);
+    if (Math.ceil(scrolltop) <= 0) {  // 如果回到了顶部，就清除当前计时函数
+        clearInterval(this.timer);
+    };
+  }, 10)  // 每 10 毫秒调用一次，这样绝对顺滑
+}
