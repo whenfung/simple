e2e5e3870f5f5d6f7f26de5fb82fc4aa91689290
peofessordeng <2248262060@qq.com>
2020-05-17 22:24:27 +0800

    图片居中，表格居中实现

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@
 window.onscroll = function() {
   var t = document.documentElement.scrollTop || document.body.scrollTop;
   var top = document.getElementsByClassName("top");
-  if( t > 300 ) { 
-    top[0].style.display = "block";
-  } else {
-    top[0].style.display = "none";
+  if(top.length > 0) {   // 有时候没有获取到 top
+    if( t > 300 ) { 
+      top[0].style.display = "block";
+    } else {
+      top[0].style.display = "none";
+    }
   }
 }
 
@@ -20,3 +22,35 @@ function toScroll() {
     };
   }, 10)  // 每 10 毫秒调用一次，这样绝对顺滑
 }
+
+// 文章内点击图片居中
+window.onload = toCenter;
+window.onresize = toCenter;
+
+function toCenter() {
+  var post = document.getElementsByClassName("post");
+  if (post.length > 0) {
+    var imgs = document.getElementsByTagName("img");
+    for(var i = 0; i < imgs.length; i ++) {
+      if(imgs[i].offsetWidth <= post[0].offsetWidth){
+        imgs[i].style.marginLeft = (post[0].offsetWidth - imgs[i].offsetWidth) / 2 + "px";
+      }
+    }
+    
+    var tables = document.getElementsByTagName("table");
+    for(var i = 0; i < tables.length; i++) {
+      tables[i].style.marginLeft = 0;   // 初始化
+      var trWidth = tables[i].getElementsByTagName("tr")[0].offsetWidth;
+      if(trWidth == post[0].offsetWidth) {
+        continue;
+      } 
+      else if (trWidth > post[0].offsetWidth) {      
+        tables[i].style.width = "100%"; // 还原
+      } 
+      else {  // 居中
+        tables[i].style.width = trWidth + 1 + "px";
+        tables[i].style.marginLeft = (post[0].offsetWidth - trWidth) / 2 + "px"; 
+      }
+    }
+  }
+};
