5fa48044c32b62d92d91769c9a604f57c7f12fa7
peofessordeng <2248262060@qq.com>
2020-05-20 18:11:52 +0800

    添加目录定位误差

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
   // 是否显示回到顶部按钮
   var t = document.documentElement.scrollTop || document.body.scrollTop;
   var top = document.querySelector(".top");  // 获取第一个绑定 top 的元素
-  if( t > 300 ) { 
+  if(t > 300 ) { 
     top.style.display = "block";
   } else {
     top.style.display = "none";
@@ -13,11 +13,15 @@ window.onscroll = function() {
   var subTitles = document.querySelectorAll('.post h2, .post h3'); // 所有标题
   
   for (var i = 0; i < subTitles.length; i++) {
-    if (subTitles[i].offsetTop + 1 >= t) {             // 可视窗口的第一个标题
+    if (subTitles[i].offsetTop + 2 >= t) {         // 可视窗口及下方的第一个标题
       if (document.querySelector('.tocactive')) {  // 清除原有高亮
           document.querySelector('.tocactive').classList.remove('tocactive');
       }
-      document.querySelector('#markdown-toc-' + subTitles[i].textContent).classList.add('tocactive');
+      if(i > 0 && subTitles[i].offsetTop - 2 >= t) {  // 标题上方还有一些内容
+        document.querySelector('#markdown-toc-' + subTitles[i-1].textContent).classList.add('tocactive');
+      } else {
+        document.querySelector('#markdown-toc-' + subTitles[i].textContent).classList.add('tocactive');
+      }
       break;
     }
   }
